4ef6ca387f2116685591ffc38ecfb8f2c6986543
Winai Nadee <winain@gmail.com>
2024-01-17 16:01:33 +0700

    deploy

---
+++ assets/js/18.2429967d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[18],{303:function(t,s,n){"use strict";n.r(s);var e=n(16),o=Object(e.a)({},(function(){var t=this._self._c;return t("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[t("List",{attrs:{folder:"courses"}})],1)}),[],!1,null,null,null);s.default=o.exports}}]);
